5c6019b17e7db9f06f025cab1fecb933b29aaa2d
EnricoBreg <55693007+EnricoBreg@users.noreply.github.com>
2022-04-22 19:31:31 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+This is a solution to the [Tip Calculator App challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Advice Generator App](#The-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new tip by clicking the button
+
+
+### Screenshot
+
+![](.screenshots/screenshot_1.png)
+![](.screenshots/screenshot_2.png)
+
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/EnricoBreg/Tip_Calculator_App.git)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Javascript Vanilla (DOM Manipulation)
+
+## Useful resources
+
+- [JavaScript DOM Crash Course (by TraversyMedia)](https://www.youtube.com/watch?v=0ik6X4DJKCc) - This helped me for DOM Manipulation in Vanilla Javascript. I really liked this video and Brad Traversy is one of my mentor :).
+
+## Author
+
+- Website - [Enrico Bregoli](https://www.your-site.com)
+- Frontend Mentor - [@EnricoBreg](https://www.frontendmentor.io/profile/EnricoBreg)
+- LinkedIn - [Enrico Bregoli](https://www.linkedin.com/in/enrico-bregoli/)
---
+++ index.js
@@ -0,0 +1,104 @@
+let peopleNumber = 0.0, billImport = 0.0;
+const presetTipButtons = document.querySelectorAll("button.preset-tip-button");
+
+let tip = {
+    percentage: 0.0,
+    isSet: false,
+    setTip: function (percentage) {
+        this.percentage = percentage;
+        this.isSet = true;
+    }
+};
+
+document.querySelector("#bill-amount").addEventListener("input", getBillImport);
+document.querySelector("button#reset").addEventListener("click", getTipAndTotal);
+document.querySelector("#custom-tip").addEventListener("input", getCustomTip);
+document.querySelector("#people-number").addEventListener("input", getPeopleNumber);
+
+presetTipButtons.forEach((button) => {
+    button.addEventListener("click", getPresetTip);
+});
+
+function getBillImport(e) {
+    if (e.target.value <= 0) {
+        e.target.value = "";
+        document.querySelector("section.bill-total-amount span.error").textContent = "Inser A Valid Number!";
+        document.querySelector("#bill-amount").classList.add("error-outline");
+    } else {
+        billImport = Number(e.target.value);
+    }
+
+    setTimeout(() => {
+        document.querySelector("section.bill-total-amount span.error").textContent = "";
+        document.querySelector("#bill-amount").classList.remove("error-outline");
+    }, 3000);
+}
+
+function getCustomTip(e) {
+    if (e.target.value > 100) {
+        e.target.value = 100;
+    } else if (e.target.value < 0) {
+        e.target.value = "";
+        document.querySelector(".container .output .tip-per-person div span.total-tip-amount").textContent = `Tip Amount (5%)`;
+    } else {
+        tip.setTip(Number(e.target.value / 100));
+    }
+    document.querySelector(".container .output .tip-per-person div span.total-tip-amount").textContent = `Tip Amount (${e.target.value}%)`;
+}
+
+function getPeopleNumber(e) {
+    if (e.target.value <= 0) {
+        e.target.value = "";
+        document.querySelector("section.people-number span.error").textContent = "Insert A Valid Number!";
+        document.querySelector("#people-number").classList.add("error-outline");
+    } else {
+        peopleNumber = Number(e.target.value);
+    }
+
+    setTimeout(() => {
+        document.querySelector("section.people-number span.error").textContent = "";
+        document.querySelector("#people-number").classList.remove("error-outline");
+    }, 3000);
+}
+
+function getPresetTip(e) {
+    switch (e.target.textContent) {
+        case "5%":
+            tip.setTip(0.05);
+            break;
+        case "10%":
+            tip.setTip(0.10);
+            break;
+        case "15%":
+            tip.setTip(0.15);
+            break;
+        case "25%":
+            tip.setTip(0.25);
+            break;
+        case "50%":
+            tip.setTip(0.5);
+            break;
+        default:
+            break;
+    }
+    document.querySelector(".container .output .tip-per-person div span.total-tip-amount").textContent = `Tip Amount (${e.target.textContent})`;
+}
+
+function getTipAndTotal() {
+    let tipAmountPerPerson, total;
+    
+    if (tip.isSet === false) {
+        document.querySelector("section.select-tip").classList.add("error-border");
+        document.querySelector("section.select-tip span.error").textContent = "Select A Tip";
+        setTimeout(() => {
+            document.querySelector("section.select-tip").classList.remove("error-border");
+            document.querySelector("section.select-tip span.error").textContent = "";
+        }, 3000);
+    }
+
+    tipAmountPerPerson = (billImport / peopleNumber) * tip.percentage;
+    total = (billImport / peopleNumber) + tipAmountPerPerson;
+    document.querySelector("#tip-per-person").textContent = `$ ${tipAmountPerPerson.toFixed(2)}`;
+    document.querySelector("#total-per-person").textContent = `$ ${total.toFixed(2)}`;
+    
+}
